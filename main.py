810f8d6eaf084e0b9d2ce3ab1a2214de806431d2
Pavel Koryakin <koryakinp@koryakinp.com>
2020-04-02 10:59:19 -0400

    -implemented video/image recording

--- main.py
+++ main.py
@@ -1,27 +1,99 @@
 import serial
 import time
 import uuid
+import os, sys
+import json
+import asyncio
+from azure.storage.blob.aio import BlobClient
 from picamera import PiCamera
 
-if __name__ == '__main__':
+def receive_message(msg, ser):
+    message_received = False
+    while not message_received:
+        if ser.in_waiting > 0:
+            line = ser.readline().decode('utf-8').rstrip()
+            print(line)
+            if(line == msg):
+                print("RECEIVED INCOMING MESSAGE: " + line)
+                message_received = True
+
+def send_message(msg, ser):
+    print("Sending message: " + msg)
+    msg = msg + "\n"
+    ser.write(msg.encode())
+
+def record_video(camera, filename):
+    path = '/home/pi/Desktop/throws/' + filename
+
+    print('Recording Video Start')
+
+    camera.start_recording(path + '.h264', quality=25)
+    time.sleep(3)
+    camera.stop_recording()
+
+    print('Recording Video End')
+
+    print('Converting Video to .mp4 Start')
+    os.system('MP4Box -add ' + path + '.h264:fps=25 ' + path +'.mp4')
+    print('Converting Video to .mp4 End')
+    
+    os.remove(path + '.h264')
+
+def capture_image(camera, filename):
+    camera.capture('/home/pi/Desktop/throws/' + filename + '.jpg')
+
+async def upload_file(container, filename, cs):
+
+    print('Uploading Blob Start')
+    blob = BlobClient.from_connection_string(
+        conn_str=cs, 
+        container_name=container, 
+        blob_name=os.path.basename(filename))
+
+    with open(filename, "rb") as data:
+        await blob.upload_blob(data)
+    print('Uploading Blob End')
+
+async def main():
+
+    experiment_id = str(uuid.uuid4())
+
+    with open('config.json') as f:
+        d = json.load(f)
+        cs = d['BlobConnectionString']
+
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
     camera = PiCamera()
+    camera.resolution = (480, 480)
+    camera.framerate = 90
+    camera.color_effects = (128,128)
+
+    receive_message("ARDUINO:COMMUNICATION HANDSHAKE", ser)
+    send_message("PI:COMMUNICATION HANDSHAKE", ser)
 
     while True:
+        receive_message("ARDUINO:READY TO THROW", ser)
 
-        id = str(uuid.uuid4())
-        ser.write(b"THROW\n")
+        send_message("PI:THROW", ser)
+        
+        throw_id = str(uuid.uuid4())
 
-        filename = '/home/pi/Desktop/throws/' + id
+        filename = experiment_id + '-' + throw_id
 
-        camera.resolution = (480, 480)
-        camera.framerate = 90
-        camera.color_effects = (128,128)
+        record_video(camera, filename)
+        capture_image(camera, filename)
 
-        camera.start_recording(filename + '.h264', quality=25)
-        time.sleep(5)
-        camera.stop_recording()
-        camera.capture(filename + '.jpg')
+        send_message("PI:RELOAD", ser)
 
-        break
+        await upload_file(
+            'auto-yahtzee-raw-image-container', 
+            '/home/pi/Desktop/throws/' + filename+ '.jpg', cs)
+
+        await upload_file(
+            'auto-yahtzee-raw-video-container',
+            '/home/pi/Desktop/throws/' + filename + '.mp4', cs)
+
+        send_message("PI:RELOAD", ser)
+
+asyncio.run(main())
